64ea8fe727e9f114688b8bcb8333ec36c9952371
Kyle Allan <kyle@stitchdata.com>
2018-03-22 13:56:52 -0400

    bump to 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='tap-bronto',
-    version='0.0.1',
+    version='0.0.2',
     description='Singer.io tap for extracting data from the Bronto API',
     author='Stitch',
     url='https://singer.io',
