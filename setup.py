27fe64d0a24f0b49f6a25776b70f8dff1b9bb7ea
Tim Visher <tim@stitchdata.com>
2018-07-31 11:00:49 -0400

    1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='tap-bronto',
-    version='1.0.0',
+    version='1.0.1',
     description='Singer.io tap for extracting data from the Bronto API',
     author='Stitch',
     url='https://singer.io',
